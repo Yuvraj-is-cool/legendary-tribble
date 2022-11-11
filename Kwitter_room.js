64b756d16afe9d05595312326c4a6ebe9b6f71ef
Yuvraj-is-cool <99299241+Yuvraj-is-cool@users.noreply.github.com>
2022-11-10 18:38:19 -0800

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAP7cQ9KvnyRRfDT4DXXA_C9zWFYZJC6a0",
+    authDomain: "kwitter-project-63152.firebaseapp.com",
+    projectId: "kwitter-project-63152",
+    storageBucket: "kwitter-project-63152.appspot.com",
+    messagingSenderId: "680530736411",
+    appId: "1:680530736411:web:83ad7a7aa732da3323cfbe"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
